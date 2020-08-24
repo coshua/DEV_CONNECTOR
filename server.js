9ce030e50e54acd67bd8246f673bedf062204548
Dongju <bitlez3470@gmail.com>
2020-08-24 15:08:13 +0900

    authorization

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const c = require("config");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+//Connect Database
+connectDB();
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
   return res.send("API running ~");
 });
+
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
