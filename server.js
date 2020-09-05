7e1494becfb1d1d1517cb94965b83ef3da0ddf60
Dongju <bitlez3470@gmail.com>
2020-09-05 17:48:43 +0900

    dev

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const c = require("config");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
+const path = require("path");
 
 //Connect Database
 connectDB();
 
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => {
-  return res.send("API running ~");
-});
 
 //Define routes
 app.use("/api/users", require("./routes/api/users"));
@@ -19,4 +16,13 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+//Serve static assets
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
